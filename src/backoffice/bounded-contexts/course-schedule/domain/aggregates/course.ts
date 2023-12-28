279a563b1c9dfba3a3ff98275676473284291871
Jesus Marenco <jesusmarencodev@gmail.com>
2023-12-28 17:55:26 -0500

    aggregate root course

--- src/backoffice/bounded-contexts/course-schedule/domain/aggregates/course.ts
+++ src/backoffice/bounded-contexts/course-schedule/domain/aggregates/course.ts
@@ -1,3 +1,62 @@
-import { AggregateRoot } from "@nestjs/cqrs";
+import { AggregateRoot } from '@nestjs/cqrs';
 
-export class Course extends AggregateRoot {}
+export type CourseEssential = {
+  readonly id: string;
+  readonly name: string;
+};
+
+export type CourseOptional = {
+  readonly summary: string;
+};
+
+export type CourseUpdate = {
+  readonly name: string;
+  readonly summary: string;
+};
+
+export type CourseProperties = Required<CourseEssential> &
+  Partial<CourseOptional>;
+
+export class Course extends AggregateRoot {
+  private readonly id: string;
+  private name: string;
+  private active: boolean;
+  private summary: string;
+  private readonly createdAt: Date;
+  private updatedAt: Date;
+  private deleteAt: Date;
+
+  constructor(properties: CourseProperties) {
+    super();
+    Object.assign(this, properties);
+    this.createdAt = new Date();
+    this.active = true;
+  }
+
+  properties() {
+    return {
+      id: this.id,
+      name: this.name,
+      active: this.active,
+      summary: this.summary,
+      createdAt: this.createdAt,
+      deleteAt: this.deleteAt,
+    };
+  }
+
+  update(fields: Partial<CourseUpdate>) {
+    Object.assign(this, fields);
+    this.updatedAt = new Date();
+  }
+
+  delete() {
+    this.active = false;
+    this.deleteAt = new Date();
+  }
+}
+
+const properties: CourseProperties = {
+  id: 'abc',
+  name: 'course 1',
+  summary: 'updated 1',
+};
